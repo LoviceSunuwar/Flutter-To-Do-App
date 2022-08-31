3274d3701b9c77c05f6712c22b844fc3eeba4f88
LoviceSunuwar <lovicesunuwar@gmail.com>
2022-08-31 00:08:43 -0400

    added appbar and colors, text, and font family

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,9 +8,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late double _deviceheight;
+  late double _devicewidth;
   _HomePageState();
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    _deviceheight = MediaQuery.of(context).size.height;
+    _devicewidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceheight * 0.13,
+        title: const Text(
+          "Taskly!",
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+      ),
+    );
   }
 }
