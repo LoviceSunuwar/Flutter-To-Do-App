e769e7cf86bccc121370f36be0921889f960cb19
LoviceSunuwar <lovicesunuwar@gmail.com>
2022-09-05 19:36:40 -0400

    to do application , Hive DB Persistant data completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -41,7 +41,7 @@ class _HomePageState extends State<HomePage> {
     return FutureBuilder(
         future: Hive.openBox('tasks'),
         builder: (BuildContext _context, AsyncSnapshot _snapshot) {
-          if (_snapshot.connectionState == ConnectionState.done) {
+          if (_snapshot.hasData) {
             _box = _snapshot.data;
             return _taskList();
           } else {
@@ -72,6 +72,15 @@ class _HomePageState extends State<HomePage> {
                 : Icons.check_box_outline_blank_outlined,
             color: Colors.greenAccent,
           ),
+          onTap: () {
+            task.done = !task.done;
+            _box!.putAt(_index, task.toMap());
+            setState(() {});
+          },
+          onLongPress: () {
+            _box!.deleteAt(_index);
+            setState(() {});
+          },
         );
       },
     );
